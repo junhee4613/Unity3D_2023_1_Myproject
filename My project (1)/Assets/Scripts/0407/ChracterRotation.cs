f6b80ad317272ce77a99c8b0b7b0aedf63a112d4
junhee4613 <junhee000123@naver.com>
2023-04-07 11:01:31 +0900

    블록 만들기 및 프리펩과 오브젝트 마우스로 회전, 이동 구현

---
+++ My project (1)/Assets/Scripts/0407/ChracterRotation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChracterRotation : MonoBehaviour
+{
+    float rotSpeed = 0;                          //변수 회전 속도
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10000;
+        }
+
+        transform.Rotate(0, this.rotSpeed * Time.deltaTime, 0);                         //transform은 캐릭터의 위치 컴퍼넌트
+
+        rotSpeed *= 0.99f;                                                              //Rotate 함수는 회전을 시켜주는 함수
+    }                                                                                   //프레임마다 1% 씩 속도 감소
+}
