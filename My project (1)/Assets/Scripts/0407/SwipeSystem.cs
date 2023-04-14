f093134bdcd95663cb81958380c11fd59db8b278
junhee4613 <junhee000123@naver.com>
2023-04-14 11:09:08 +0900

    슈팅게임 구현

--- My project (1)/Assets/Scripts/0407/SwipeSystem.cs
+++ My project (1)/Assets/Scripts/0407/SwipeSystem.cs
@@ -17,12 +17,16 @@ public class SwipeSystem : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0)) initialPos = Input.mousePosition;
         if (Input.GetMouseButtonUp(0)) Calculate(Input.mousePosition);
+        
     }
 
     void Calculate(Vector3 finalPos)                        //계산하는 함수를 만든다.
     {
         float disX = Mathf.Abs(initialPos.x - finalPos.x);          //Mathf.abs 함수는 절대값을 구하는 함수 (거리를 구해야 하기 때문에)
         float disY = Mathf.Abs(initialPos.y - finalPos.y);
+        Debug.Log("시작점: " + initialPos);
+        Debug.Log("끝나는 점: " + finalPos);
+        Debug.Log(disX);
 
         if(disX > 0 || disY > 0)                            // || <- or 을 나타내는 것 (&&)
         {
---
+++ My project (1)/Assets/Scripts/Controller/MonsterController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    public int Monster_hp = 5;
+
+    public void Monster_Damaged(int damage)     //데미지 받는 함수 (인수 int)
+    {
+        Monster_hp -= damage;                   //데미지를 뺀다
+
+        if(Monster_hp <= 0)
+        {
+            GameObject temp = this.gameObject;      //나 자신을 가져와서 temp에 입력한다.
+            Destroy(temp);
+        }
+    }
+}
