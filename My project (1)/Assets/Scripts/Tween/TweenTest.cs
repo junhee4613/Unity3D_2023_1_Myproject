488a9417b68c81d4110b41d2bc835c04ad93387c
junhee4613 <junhee000123@naver.com>
2023-05-15 01:34:23 +0900

    닷트윈 복습 및 설명 추가

--- My project (1)/Assets/Scripts/Tween/TweenTest.cs
+++ My project (1)/Assets/Scripts/Tween/TweenTest.cs
@@ -14,10 +14,10 @@ public class TweenTest : MonoBehaviour
         //transform.DORotate(new Vector3(0, 0, 180), 2); //z축으로 180도 회전
         //transform.DOScale(new Vector3(2, 2, 2), 2);     //2초 동안 스케일이 2배로 커짐
 
-        Sequence sequence = DOTween.Sequence();
-        sequence.Append(transform.DOMoveX(5, 2).SetEase(Ease.OutBounce));
-        sequence.Append(transform.DORotate(new Vector3(0, 0, 180), 2));
-        sequence.Append(transform.DOScale(new Vector3(2, 2, 2), 2).OnComplete(SequenceEnd));
+        //sequenceLoop = DOTween.Sequence();
+        //sequenceLoop.Append(transform.DOMoveX(5, 2).SetEase(Ease.OutBounce));
+        //sequenceLoop.Append(transform.DORotate(new Vector3(0, 0, 180), 2));
+        //sequenceLoop.Append(transform.DOScale(new Vector3(2, 2, 2), 2).OnComplete(SequenceEnd));
 
         //Sequence sequence = DOTween.Sequence();
         //sequence.Append(transform.DOMoveX(5, 2).SetEase(Ease.OutBounce));
@@ -33,12 +33,12 @@ public class TweenTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             if (!isPunch)
             {
-                isPunch = true;
-                transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), 0.1f, 10, 1).OnComplete(EndPunch);
+                isPunch = true;                    //크기           
+                transform.DOPunchScale(new Vector3(5, 5, 5), 10f, 10, 10).OnComplete(EndPunch);
             }
         }
         
